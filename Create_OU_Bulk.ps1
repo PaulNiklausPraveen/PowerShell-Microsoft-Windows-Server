8431933e68afb789c98c822e8f259728ad527255
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2022-09-12 22:22:24 +0530

    Create Create_OU_Bulk.ps1

---
+++ Create_OU_Bulk.ps1
@@ -0,0 +1,18 @@
+
+#This script is used to create organizational units in bulk
+
+#Import active directory module
+Import-Module activedirectory
+
+#Import the data from CSV file.
+$OUData = Import-csv C:\temp\oufile.csv
+
+#Loop through each row containing  details in CSV file
+foreach ($ou in $OUData)
+{
+
+$ouname = $ou.name
+$oupath = $ou.path
+
+New-ADOrganizationalUnit -Name $ouname -path $oupath 
+}
